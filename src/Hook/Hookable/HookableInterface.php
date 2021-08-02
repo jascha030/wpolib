05b8d78ec8f8e0bcb0290e0c7605a514633044f5
Jascha030 <contact@jaschavanaalst.nl>
2021-08-03 01:21:21 +0200

    HookableInterface

--- src/Hook/Hookable/HookableInterface.php
+++ src/Hook/Hookable/HookableInterface.php
@@ -1,1 +1,26 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Jascha030\Wpolib\Hook\Hookable;
+
+/**
+ * Used to indicate a class containing methods,
+ * which shall be hooked to a Wordpress action/filter hook using the Plugin Api.
+ */
+interface HookableInterface
+{
+    /**
+     * Returns predefined action/method hooks,
+     * which can be loaded lazily.
+     * @return array
+     */
+    public static function getActions(): array;
+
+    /**
+     * Returns predefined filter/method hooks,
+     * which can be loaded lazily.
+     * @return array
+     */
+    public static function getFilters(): array;
+}
