038c3a8c45f299f6b268d025f6c3600be4a705d5
Jascha030 <contact@jaschavanaalst.nl>
2021-08-03 21:37:26 +0200

    Psr11 wrapper for pimple container

---
+++ src/Configuration/Psr11/Psr11PimpleContainer.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @noinspection PhpMethodNamingConventionInspection
+ * @noinspection PhpClassNamingConventionInspection
+ */
+
+declare(strict_types=1);
+
+namespace Jascha030\Wpolib\Configuration\Psr11;
+
+use Jascha030\Wpolib\Exception\Psr11\ContainerEntryNotFoundException;
+use Pimple\Container;
+use Psr\Container\ContainerInterface;
+
+final class Psr11PimpleContainer implements ContainerInterface
+{
+    private Container $container;
+
+    /**
+     * @param \Pimple\Container $container
+     */
+    public function __construct(Container $container)
+    {
+
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get(string $id)
+    {
+        if (!$this->has($id)) {
+            throw new ContainerEntryNotFoundException($id);
+        }
+
+        return $this->container[$id];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has(string $id): bool
+    {
+        return isset($this->container[$id]);
+    }
+}
