fef8316ebe8415c86a3c8d180f1b654e9e40881f
Jascha030 <contact@jaschavanaalst.nl>
2021-08-03 14:46:49 +0200

    Simplified HookRegistry class

--- src/Hook/HookRegistry.php
+++ src/Hook/HookRegistry.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Wpolib\Hook;
 
 use Jascha030\Wpolib\Exception\DoesNotImplementException;
-use Jascha030\Wpolib\Hook\Hookable\HookableInterface;
+use Jascha030\Wpolib\Hook\Hookable\Hookable;
+use Jascha030\Wpolib\Hook\Hookable\InvokeHookable;
 use Psr\Container\ContainerInterface;
 
 class HookRegistry
@@ -36,16 +39,6 @@ class HookRegistry
     }
 
     /**
-     * Hooks all hookables.
-     *
-     * @throws \Jascha030\Wpolib\Exception\DoesNotImplementException
-     */
-    public function run(): void
-    {
-        $this->hookLazyReferences();
-    }
-
-    /**
      * Wraps class and method in a Closure that calls our container and checks if our hook identifier still exists,
      * In that case, the Class is retrieved from our container to execute hooked method,
      * If not, the method  wil not be executed. This is a workaround for newer wordpress versions,
@@ -62,36 +55,41 @@ class HookRegistry
     /**
      * Get predefined actions/filters statically
      * Based on this, wrap and hook all methods.
-     *
      * @throws \Jascha030\Wpolib\Exception\DoesNotImplementException
      */
-    private function hookLazyReferences(): void
+    public function hookClasses(): void
     {
-        foreach (array_keys($this->hookables) as $className) {
-            if (!is_subclass_of($className, HookableInterface::class)) {
-                throw new DoesNotImplementException($className, HookableInterface::class);
+        foreach (array_keys($this->hookables) as $hookableClass) {
+            if (
+                ! is_subclass_of($hookableClass, Hookable::class) &&
+                ! is_subclass_of($hookableClass, InvokeHookable::class)
+            ) {
+                throw new DoesNotImplementException(
+                    $hookableClass,
+                    Hookable::class . ' or ' . InvokeHookable::class
+                );
             }
 
-            $this->hookClassMethods($className);
+            $this->hookClassMethods($hookableClass, is_subclass_of($hookableClass, Hookable::class));
         }
     }
 
-    /**
-     * @noinspection NotOptimalIfConditionsInspection
-     */
-    private function hookClassMethods(string $hookableClass): void
+    private function hookClassMethods(string $hookableClass, bool $lazy): void
     {
         $methods = ['getActions', 'getFilters'];
 
-        foreach ($methods as $key => $method) {
-            $hooks     = $hookableClass::{$method}();
-            $addMethod = 'add_'.self::FILTER_TYPES[$key];
+        foreach ($methods as $filterType => $hookMethod) {
+            $hooks = $lazy
+                ? $hookableClass::{$hookMethod}()
+                : $hookableClass->{$hookMethod}();
 
-            foreach ($hooks as $tag => $parameters) {
+            $addMethod = 'add_' . self::FILTER_TYPES[$filterType];
+
+            foreach ($hooks as $filterTag => $parameters) {
                 if (\is_array($parameters) && \is_array($parameters[0])) {
-                    $this->hookMultipleMethods($parameters, $addMethod, $tag, $hookableClass);
+                    $this->hookMethods($parameters, $addMethod, $filterTag, $hookableClass);
                 } else {
-                    $this->hookMethod($parameters, $addMethod, $tag, $hookableClass);
+                    $this->hookMethod($parameters, $addMethod, $filterTag, $hookableClass);
                 }
             }
         }
@@ -102,13 +100,10 @@ class HookRegistry
      *
      * @param mixed $tag
      */
-    private function hookMultipleMethods(array $parameters, string $addMethod, $tag, string $serviceClass): void
+    private function hookMethods(array $parameters, string $addMethod, $tag, string $serviceClass): void
     {
         foreach ($parameters as $params) {
-            if (!\is_array($params)) {
-                $params = [$params];
-            }
-
+            $params = ! \is_array($params) ? [$params] : $params;
             $this->hookMethod($params, $addMethod, $tag, $serviceClass);
         }
     }
@@ -116,15 +111,16 @@ class HookRegistry
     /**
      * Hook a single method defined in a hookable class.
      *
-     * @param $parameters
-     * @param $tag
+     * @param        $parameters
+     * @param string $addMethod
+     * @param        $tag
+     * @param string $serviceClass
      */
     private function hookMethod($parameters, string $addMethod, $tag, string $serviceClass): void
     {
-        if (!\is_array($parameters)) {
-            $parameters = [$parameters];
-        }
+        $parameters = ! \is_array($parameters) ? [$parameters] : $parameters;
+        $callable   = $this->wrap($serviceClass, array_shift($parameters));
 
-        $addMethod($tag, $this->wrap($serviceClass, array_shift($parameters)), ...$parameters);
+        $addMethod($tag, $callable, ...$parameters);
     }
 }
