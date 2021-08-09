be3d5fab7de3210232ae89abb5d54b5dc4c802d4
Jascha030 <contact@jaschavanaalst.nl>
2021-08-09 22:53:35 +0200

    ConfigInterface

--- src/Container/Config/ConfigInterface.php
+++ src/Container/Config/ConfigInterface.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace SocialBrothers\WPUtils\Container\Config;
+namespace Jascha030\Wpolib\Container\Config;
 
 use Pimple\Container as PimpleContainer;
 
 interface ConfigInterface
 {
     public function configure(PimpleContainer $container): void;
+
+    public function getConfigArray(): array;
+
+    public function getHookables(): array;
+
+    public function getProviders(): array;
 }
